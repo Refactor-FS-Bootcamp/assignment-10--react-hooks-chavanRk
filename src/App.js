7f9feea74d22c216272fd52d45a8822eb3a86e76
Rohitkumar Chavan <109240176+chavanRk@users.noreply.github.com>
2022-12-15 18:46:51 +0530

    Assignment-10

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import Hooksapi from './components/Hooksapi';
+
+function App() {
+  return (
+    <div className="App">
+      <Hooksapi />
+    </div>
+  );
+}
+
+export default App;
